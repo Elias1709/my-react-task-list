c24b89aefa49ca3f857b36ca8c5f64d617555b65
Elias <eliias1709gmail.com>
2023-09-14 17:22:07 -0500

    editando

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TaskList } from './components/TaskList'
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   
 
   return (
     <div>
-      <TaskList />
+      <ChakraProvider>
+        <TaskList />
+      </ChakraProvider>
+      
     </div>
   )
 }
